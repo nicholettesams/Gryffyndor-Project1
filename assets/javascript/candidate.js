41df21b5d131152a27f5d3771d36016d2a012ad7
Nicholette Sams <nicholettesams@msn.com>
2018-09-18 09:59:56 -0400

    Format date, make headline the URL

--- assets/javascript/candidate.js
+++ assets/javascript/candidate.js
@@ -47,9 +47,6 @@ var updatePage = function(NYTData) {
     for (var i = 0; i < 10; i++) {
         var article = NYTData.response.docs[i];
 
-        // Increase the articleCount (track article # - starting at 1)
-        var articleCount = i + 1;
-
         // Create the  list group to contain the articles and add the article content for each
         var $articleList = $("<ul>");
         $articleList.addClass("list-group");
@@ -62,38 +59,30 @@ var updatePage = function(NYTData) {
         var $articleListItem = $("<li class='list-group-item articleHeadline'>");
 
         if (headline && headline.main) {
-        console.log(headline.main);
-        $articleListItem.append(
-            "<span class='label label-primary'>" +
-            articleCount +
-            "</span>" +
-            "<strong> " +
-            headline.main +
-            "</strong>"
-        );
+            console.log(headline.main);
+            $articleListItem.append(
+            "<a href='" + article.web_url + "' target='_blank'><strong>" + headline.main + "</strong></a>");
         }
 
         // Append a byline to document if exists
         var byline = article.byline;
         if (byline && byline.original) {
-        $articleListItem.append("<h6>" + byline.original + "</h6>");
+            $articleListItem.append("<h6>" + byline.original + "</h6>");
         }
 
         // Append section to document if exists
         var section = article.section_name;
         if (section) {
-        $articleListItem.append("<h6>Section: " + section + "</h6>");
+            $articleListItem.append("<h6>Section: " + section + "</h6>");
         }
 
         // Append published date to document if exists
-        // TODO: need to format date
-        var pubDate = article.pub_date;
+        var pubDate = article.pub_date
+        console.log(pubDate)
         if (pubDate) {
-        $articleListItem.append("<h6>" + article.pub_date + "</h6>");
+            pubDate = dateFns.format(pubDate, "YYYY-MM-DD");
+            $articleListItem.append("<h6>" + pubDate + "</h6>");
         }
-
-        // Append and url
-        $articleListItem.append("<a href='" + article.web_url + "'>" + "Full Article" + "</a>");
 
         // Append the article
         $articleList.append($articleListItem);
