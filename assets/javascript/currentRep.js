94e6dba92f48e1eb7947f270fd56fe51806dd7c6
unknown <sarah.lopriore@gmail.com>
2018-09-18 19:55:11 -0400

    adding spacing fix

--- assets/javascript/currentRep.js
+++ assets/javascript/currentRep.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 
-    var address = "&address=30008%20Shadow%20Creek%20Dr%20Westlake%20OH%2044145";
+    var address = $("#").val();
+    address = address.replace(" ", "%20");
+    //var address = "30008%20Shadow%20Creek%20Dr%20Westlake%20OH%2044145";
     var apiKey = "AIzaSyCMZgdTS5Ln1SHuAC1n9QnpwMADHeKF02k";
-    var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + apiKey + address
+    var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + apiKey + "&address=" + address;
     console.log(queryURL);
+
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(queryURL);
         console.log(response);
+        console.log(response.officials[5].name);
+        console.log(response.officials[5].party);
+        console.log(response.officials[5].photoUrl);
+        presidentDiv = $("<div></div>");
+        nameP = $("<p></p>");
+        nameP.text(response.officials[5].name);
+        presidentDiv.append(nameP);
     })
 
 })
